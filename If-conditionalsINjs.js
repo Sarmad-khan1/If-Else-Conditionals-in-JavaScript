c4f2e19e3879cfa4ef4b08c510815b0c5f556172
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-12-30 13:44:31 +0500

    Add files via upload

---
+++ If-conditionalsINjs.js
@@ -0,0 +1,20 @@
+//Simple If-Else Conditionals
+
+<script>
+let v = 1000;
+    if(v ===100){
+        console.log('V is equal to 100');
+    }
+    else{
+        console.log('V is not equal to 100');
+    }
+
+let vv = 100;
+    if(vv ===100){
+        console.log('VV is equal to 100');
+    }
+    else{
+        console.log('VV is not equal to 100');
+    }
+
+    </script>
